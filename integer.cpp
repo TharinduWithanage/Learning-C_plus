408e16d97a8d3253d67c97e9f96b6aa07c6948a0
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2019-10-22 18:19:13 +0530

    how to learning c++

---
+++ integer.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int myage=22;
+	cout<< "I am " <<myage <<" years old. ";
+	return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x,y,sum;
+	cout<<"Enter the number 1 ";
+	cin>>x;
+	cout<<"Enter the number 2 ";
+	cin>>y;
+	sum=x+y;
+	cout<< "sumation is "<<sum;
+}
