69bcc096f2af4ad17ee581fc03a461891e99165a
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2019-10-26 21:10:18 +0530

    slit hack

---
+++ slitovernight3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int x,i;
+	cin>> x;
+	for(i=2; i!=x; i++)
+	{
+		cout << "GetMin(int,";
+	}
+	
+	cout<<"GetMin(int,int)";
+	for(int i=2; i!=x; i++)
+	{
+		cout<<")";
+	}
+	return 0;
+}
